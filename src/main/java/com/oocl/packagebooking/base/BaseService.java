0f14e23d4b0128db99c2ecefac0b2fe336955564
“Dean” <467271883@qq.com>
2019-07-25 10:02:12 +0800

    Add BaseModule

---
+++ src/main/java/com/oocl/packagebooking/base/BaseService.java
@@ -0,0 +1,21 @@
+package com.oocl.packagebooking.base;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public interface BaseService<T, ID> {
+
+    T save(T t);
+    T update(T t);
+    List<T> saveAll(List<T> ts);
+    List<T> findAll();
+    List<T> findAll(Sort sort);
+    Page<T> findAll(int page, int size);
+    Page<T> findAll(int page);
+    T findById(ID id);
+    Page<T> findAllByKeyword(Example<T> example, int page, int size, Sort sort);
+    void delete(T t);
+}
